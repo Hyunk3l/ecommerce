16ce19a60715aac0cd2dcf08e9186f075061de67
Fabrizio Di Napoli <fabri@MacBook-Pro-de-Fabrizio.local>
2021-04-25 09:20:16 +0200

    Basic project

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "ecommerce"
+
+include("acquisition")
+include("inventory")
+include("notification")
+include("payment")
+include("order")
+include("search")
+include("shipping")
+include("shopping")
