f6aca46173b071dd3f65c607409f70f27e745a74
Fabrizio Di Napoli <hyunk3l@gmail.com>
2022-02-05 10:45:37 +0100

    Test shopping cart

---
+++ shopping/src/test/kotlin/com/fabridinapoli/shopping/domain/model/ShoppingCartShould.kt
@@ -0,0 +1,25 @@
+package com.fabridinapoli.shopping.domain.model
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.util.UUID
+
+class ShoppingCartShould : StringSpec({
+    "add a product to a new cart" {
+        val productId = ProductId(UUID.randomUUID().toString())
+        val shoppingCart = ShoppingCart(
+            id = ShoppingCartId(id = UUID.randomUUID()),
+            userId = UserId(value = UUID.randomUUID()),
+            products = emptyList(),
+        )
+        val shoppingCartWithOneProduct = ShoppingCart(
+            id = shoppingCart.id,
+            userId = shoppingCart.userId,
+            products = listOf(productId),
+        )
+
+        val updatedShoppingCart = shoppingCart.addProduct(productId)
+
+        updatedShoppingCart shouldBe shoppingCartWithOneProduct
+    }
+})
