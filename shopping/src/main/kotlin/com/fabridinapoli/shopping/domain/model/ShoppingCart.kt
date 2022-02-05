540cc52bc838bf850c42673855ca4395531ad309
Fabrizio Di Napoli <hyunk3l@gmail.com>
2022-02-05 11:45:26 +0100

    Validate business rule

--- shopping/src/main/kotlin/com/fabridinapoli/shopping/domain/model/ShoppingCart.kt
+++ shopping/src/main/kotlin/com/fabridinapoli/shopping/domain/model/ShoppingCart.kt
@@ -1,16 +1,27 @@
 package com.fabridinapoli.shopping.domain.model
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import java.util.UUID
+
+private const val MAX_PRODUCTS_ALLOWED_PER_CART = 15
 
 data class ShoppingCart(val id: ShoppingCartId, val userId: UserId, val products: List<ProductId>) {
     fun addProduct(productId: ProductId): ShoppingCart = this.copy(products = this.products + productId)
 
     companion object {
-        fun from(id: String, userId: String, products: List<ProductId>) = ShoppingCart(
-            id = ShoppingCartId.from(id),
-            userId = UserId.from(userId),
-            products = products
-        )
+        fun from(id: String, userId: String, products: List<String>): Either<DomainError, ShoppingCart> {
+            if (products.count() > MAX_PRODUCTS_ALLOWED_PER_CART) {
+                return DomainError("Too many products. Max allowed is $MAX_PRODUCTS_ALLOWED_PER_CART").left()
+            }
+
+            return ShoppingCart(
+                id = ShoppingCartId.from(id),
+                userId = UserId.from(userId),
+                products = products.map { ProductId(it) }
+            ).right()
+        }
     }
 }
 
--- shopping/src/test/kotlin/com/fabridinapoli/shopping/domain/model/ShoppingCartShould.kt
+++ shopping/src/test/kotlin/com/fabridinapoli/shopping/domain/model/ShoppingCartShould.kt
@@ -1,5 +1,8 @@
 package com.fabridinapoli.shopping.domain.model
 
+import arrow.core.flatMap
+import arrow.core.left
+import arrow.core.right
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import java.util.UUID
@@ -8,18 +11,28 @@ class ShoppingCartShould : StringSpec({
     "add a product to a new cart" {
         val productId = ProductId(UUID.randomUUID().toString())
         val shoppingCart = ShoppingCart(
-            id = ShoppingCartId.from(UUID.randomUUID().toString()),
-            userId = UserId(value = UUID.randomUUID()),
+            id = ShoppingCartId(UUID.randomUUID()),
+            userId = UserId(UUID.randomUUID()),
             products = emptyList(),
         )
         val shoppingCartWithOneProduct = ShoppingCart.from(
             id = shoppingCart.id.id.toString(),
             userId = shoppingCart.userId.value.toString(),
-            products = listOf(productId),
+            products = listOf(productId.value),
         )
 
         val updatedShoppingCart = shoppingCart.addProduct(productId)
 
-        updatedShoppingCart shouldBe shoppingCartWithOneProduct
+        updatedShoppingCart.right() shouldBe shoppingCartWithOneProduct
+    }
+
+    "fail if list of products is bigger than 15" {
+        val products = (1..16).map { UUID.randomUUID().toString() }
+
+        ShoppingCart.from(
+            id = UUID.randomUUID().toString(),
+            userId = UUID.randomUUID().toString(),
+            products = products,
+        ) shouldBe DomainError("Too many products. Max allowed is 15").left()
     }
 })
