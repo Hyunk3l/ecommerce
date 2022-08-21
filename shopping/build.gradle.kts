86041d764a4c2463558eedfcd432d1077ffb2fe4
Fabrizio Di Napoli <hyunk3l@gmail.com>
2022-08-21 19:44:12 +0200

    Add properties

--- shopping/build.gradle.kts
+++ shopping/build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+	kotlin("jvm") version "1.7.10"
+	kotlin("plugin.spring") version "1.7.10"
 }
 
 group = "com.fabridinapoli"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
