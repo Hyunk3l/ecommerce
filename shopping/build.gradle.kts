c6cf7d60212ae232a13d3e7e4390ecdaa8fba5a8
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-04-10 09:12:13 +0200

    Format

--- shopping/build.gradle.kts
+++ shopping/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.5"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.20"
-	kotlin("plugin.spring") version "1.8.20"
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.spring") version "1.8.20"
 }
 
 group = "com.fabridinapoli"
@@ -12,54 +12,54 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.2.2")
-	implementation("org.apache.kafka:kafka-streams:3.4.0")
-	implementation("io.arrow-kt:arrow-core:1.1.5")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.20")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
-	implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.2.2")
+    implementation("org.apache.kafka:kafka-streams:3.4.0")
+    implementation("io.arrow-kt:arrow-core:1.1.5")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.20")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
+    implementation("org.postgresql:postgresql")
     implementation("org.springframework:spring-tx:6.0.7")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(module = "mockito-core")
-	}
-	testImplementation("io.projectreactor:reactor-test:3.5.4")
-	testImplementation("org.testcontainers:testcontainers:1.18.0")
-	testImplementation("org.testcontainers:junit-jupiter:1.18.0")
-	testImplementation("org.testcontainers:postgresql:1.18.0")
-	testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
-	testImplementation("io.kotest:kotest-assertions-json:5.5.5")
-	testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
-	testImplementation("io.rest-assured:rest-assured:5.3.0")
-	testImplementation("io.rest-assured:json-path:5.3.0")
-	testImplementation("io.rest-assured:xml-path:5.3.0")
-	testImplementation("io.rest-assured:json-schema-validator:5.3.0")
-	testImplementation("io.mockk:mockk:1.13.4")
-	testImplementation("com.ninja-squad:springmockk:4.0.2")
-	testImplementation("net.java.dev.jna:jna:5.13.0")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("io.projectreactor:reactor-test:3.5.4")
+    testImplementation("org.testcontainers:testcontainers:1.18.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.18.0")
+    testImplementation("org.testcontainers:postgresql:1.18.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
+    testImplementation("io.kotest:kotest-assertions-json:5.5.5")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+    testImplementation("io.rest-assured:json-path:5.3.0")
+    testImplementation("io.rest-assured:xml-path:5.3.0")
+    testImplementation("io.rest-assured:json-schema-validator:5.3.0")
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    testImplementation("net.java.dev.jna:jna:5.13.0")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 extra["kotlin-coroutines.version"] = "1.6.0"
